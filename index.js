b0c4f03e9dd5f1f15ad6510376554891c1dd199c
Saksham <sakshamraj61@gmail.com>
2023-02-15 19:48:29 +0530

    origin changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "https://mybloggspace.netlify.app",
+app.use(cors({ credentials: true, origin: "*",
  }));
 app.use(express.json());
 app.use(cookieParser());
